ca7cf9d708a9b72c3d1ebd6ff6c99aeb1218dd71
muhammadmuaazmadni <muhammadmuaazmadni@gmail.com>
2021-02-01 15:16:25 +0500

    changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Hi(props){
+  return <div className="main">
+    <strong>Hello World</strong>
+    <h1>Some Info</h1>
+<ul>
+<h2>name :{props.name}</h2> 
+<h2>fname :{props.fname}</h2> 
+<h2>title :{props.title}</h2> 
+<h2>add :{props.add}{5 + 10}</h2> 
+
+</ul>
+
+  </div>
+}
+ReactDOM.render(<Hi name="Muaaz" fname="Abdul Qadir" title="Alone But Happy" add="{5+10}" />,document.querySelector("#root"))
+
